ea0f3f411720a477347ee0862eaf01a949528c26
Oemed <oal-ali@roc-dev.com>
2017-01-31 11:37:48 +0100

    tweede commit java.

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Opdracht school");
+        System.out.println("Opdracht school van oemed");
     }
 }
